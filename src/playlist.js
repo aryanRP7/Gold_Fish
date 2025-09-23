f6258e7b966109fc452d0a484c42bfa0f7a20bc8
aryanRP7 <aryanalls100@gmail.com>
2025-09-23 21:13:01 +0530

    kkSongsnew

--- src/playlist.js
+++ src/playlist.js
@@ -1,17 +1,17 @@
-import song1 from './assets/song1.mp3';
-import song2 from './assets/song2.mp3';
-import song3 from './assets/song3.mp3';
-import song4 from './assets/song4.mp3';
-import song5 from './assets/song5.mp3';
-import song6 from './assets/song6.mp3';
-import song7 from './assets/song7.mp3';
+import song1 from "./assets/song1.mp3";
+import song2 from "./assets/song2.mp3";
+import song3 from "./assets/song3.mp3";
+import song4 from "./assets/song4.mp3";
+import song5 from "./assets/song5.mp3";
+import song6 from "./assets/song6.mp3";
+import song7 from "./assets/song7.mp3";
 
 export const playlist = [
-  { name: 'GoldFish', src: song1 },
-  { name: 'Paniyo sa', src: song2 },
-  { name: 'Laila', src: song3 },
-  { name: 'Raanjhanaa', src: song4 },
-  { name: 'Toota Tara', src: song5 },
-  { name: 'Mast Magan', src: song6 },
-  { name: 'K.K.', src: song7 },
-].sort((a, b) => a.name.localeCompare(b.name));
+  { name: "GoldFish", src: song1 },
+  { name: "Laila", src: song3 },
+  { name: "Mast Magan", src: song6 },
+  { name: "Paniyo sa", src: song2 },
+  { name: "Raanjhanaa", src: song4 },
+  { name: "Toota Tara", src: song5 },
+  { name: "K.K.", src: song7 },
+];

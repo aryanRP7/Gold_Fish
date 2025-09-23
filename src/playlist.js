53313b74eb76983ff1002bd5194f782026770fde
aryanRP7 <aryanalls100@gmail.com>
2025-09-23 21:01:54 +0530

    kkSongs

--- src/playlist.js
+++ src/playlist.js
@@ -4,6 +4,7 @@ import song3 from './assets/song3.mp3';
 import song4 from './assets/song4.mp3';
 import song5 from './assets/song5.mp3';
 import song6 from './assets/song6.mp3';
+import song7 from './assets/song7.mp3';
 
 export const playlist = [
   { name: 'GoldFish', src: song1 },
@@ -12,4 +13,5 @@ export const playlist = [
   { name: 'Raanjhanaa', src: song4 },
   { name: 'Toota Tara', src: song5 },
   { name: 'Mast Magan', src: song6 },
+  { name: 'K.K.', src: song6 },
 ].sort((a, b) => a.name.localeCompare(b.name));

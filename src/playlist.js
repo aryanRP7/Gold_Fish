f75bd5794db0c8db3eeaab092aa1be5d52fad60c
aryanRP7 <aryanalls100@gmail.com>
2025-09-24 16:32:11 +0530

    song 8 dekha hazaroArz kiya hai

--- src/playlist.js
+++ src/playlist.js
@@ -5,6 +5,7 @@ import song4 from "./assets/song4.mp3";
 import song5 from "./assets/song5.mp3";
 import song6 from "./assets/song6.mp3";
 import song7 from "./assets/song7.mp3";
+import song8 from "./assets/song8.mp3";
 
 export const playlist = [
   { name: "GoldFish", src: song1 },
@@ -14,4 +15,5 @@ export const playlist = [
   { name: "Raanjhanaa", src: song4 },
   { name: "Toota Tara", src: song5 },
   { name: "K.K.", src: song7 },
+  { name: "Arz", src: song8 },
 ];

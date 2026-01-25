4c2d40d0c782bb190a62e2493f9fe17579da5735
aryanRP7 <aryanalls100@gmail.com>
2026-01-24 18:04:34 -0600

    updated

--- src/playlist.js
+++ src/playlist.js
@@ -9,6 +9,7 @@ import song8 from "./assets/song8.mp3";
 import song9 from "./assets/song9.mp3";
 import song10 from "./assets/song10.mp3";
 import song11 from "./assets/song11.mp3";
+import song12 from "./assets/song12.mp3";
 
 export const playlist = [
   { name: "GoldFish", src: song1 },
@@ -22,6 +23,7 @@ export const playlist = [
   { name: "Kahani", src: song9 },
   { name: "Mann", src: song10 },
   { name: "Itni Baat", src: song11 },
+  { name: "Pal", src: song12 },
 ];
 //Git website GoldFish - https://aryanrp7.github.io/Gold_Fish/
 // 🦚

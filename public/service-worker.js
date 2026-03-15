6a66c095d8641222695f673427dfb31e52c28093
aryanRP7 <aryanalls100@gmail.com>
2026-03-15 04:01:34 -0500

    updatedMarch15icon

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "goldfish-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve cached files if offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/index.js
+++ src/index.js
@@ -14,3 +14,12 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+// Register service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("Service Worker registered:", reg))
+      .catch((err) => console.log("Service Worker registration failed:", err));
+  });
+}
